f66107cc2228aad21521b21b725a250e3290b717
kyubuns <kyubuns@gmail.com>
2018-01-08 14:18:01 +0900

    ダイスを落とす

---
+++ Assets/Scripts/Dice/Dice.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Dice
+{
+    public class Dice : MonoBehaviour
+    {
+        public void Update()
+        {
+            if (transform.position.y < -10.0f) Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Dice/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dice
+{
+	public class Spawner : MonoBehaviour
+	{
+		[SerializeField] private List<GameObject> DicePrefabs = new List<GameObject>();
+		[SerializeField] private int Min;
+		[SerializeField] private int Max;
+
+		public void Start()
+		{
+			var num = Random.Range(Min, Max);
+			for (var i = 0; i < num; i++)
+			{
+				var pos = new Vector3(0.0f, i * 0.02f, 0.0f);
+				Instantiate(DicePrefabs[Random.Range(0, DicePrefabs.Count)], pos, Quaternion.identity, transform);
+			}
+		}
+
+		public void Update()
+		{
+			Debug.Log($"Dice: {transform.childCount}");
+		}
+	}
+}
