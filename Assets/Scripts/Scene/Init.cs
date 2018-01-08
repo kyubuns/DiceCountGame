337d2b6761e8796387cd4915c7af9d3ec8e887c4
kyubuns <kyubuns@gmail.com>
2018-01-08 13:37:42 +0900

    Init->Mainシーンへの遷移

---
+++ Assets/Scripts/Scene/Init.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scene
+{
+    public class Init : MonoBehaviour
+    {
+        public void Start()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
